a7745a69696c4193993ec06dce17ee29272f3dd9
mbickers <mbickers@umich.edu>
2020-12-19 17:59:27 -0600

    Added template server code

--- pkg/client/root.go
+++ pkg/client/root.go
@@ -1,11 +1,60 @@
 package client
 
 import (
+	"bytes"
 	"fmt"
-	"github.com/evanweissburg/clippy/pkg/networking"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
 )
 
+func invalid_usage() {
+	fmt.Println("Correct usage: <>")
+	os.Exit(1)
+}
+
 func Execute() {
-	fmt.Println("Clippy client sending file!")
-	networking.SendData("test.txt")
+	if len(os.Args) != 3 {
+		invalid_usage()
+	}
+
+	switch os.Args[1] {
+	case "put":
+		filename := os.Args[2]
+		fmt.Printf("put %s\n", filename)
+
+		data := "test_data"
+		resp, err := http.Post("http://localhost:8080/", "text/plain", bytes.NewBuffer([]byte(data)))
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		log.Println(string(body))
+
+	case "get":
+		clipcode := os.Args[2]
+		fmt.Printf("Retrieving clipcode %s\n", clipcode)
+		resp, err := http.Get("http://localhost:8080/" + clipcode)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		log.Println(string(body))
+
+	default:
+		invalid_usage()
+	}
 }
--- pkg/server/root.go
+++ pkg/server/root.go
@@ -2,8 +2,15 @@ package server
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
 func Execute() {
-	fmt.Println("Clippy server")
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
 }
