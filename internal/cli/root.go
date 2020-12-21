138cba126bf3bd2fcc120b2ffb43fd563d33664f
mbickers <mbickers@umich.edu>
2020-12-21 14:21:14 -0600

    Added archiver dependency, implemented cli filesystem read/write

--- internal/cli/root.go
+++ internal/cli/root.go
@@ -3,7 +3,8 @@ package cli
 import (
 	"fmt"
 	"github.com/evanweissburg/clippy/pkg/client"
-	"io/ioutil"
+	"github.com/mholt/archiver/v3"
+	"io"
 	"log"
 	"os"
 )
@@ -21,43 +22,72 @@ func Execute() {
 	switch os.Args[1] {
 	case "put":
 		filename := os.Args[2]
-
-		file, err := os.Open(filename)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		cl := client.Client{
-			BaseURL: "http://localhost:8080/",
-		}
-
-		clipcode, err := cl.Upload(file)
-		if err != nil {
-			log.Fatalln(err)
-		}
-
-		fmt.Printf("Recieved clipcode %s\n", clipcode)
+		put(filename)
 
 	case "get":
 		clipcode := os.Args[2]
-
-		cl := client.Client{
-			BaseURL: "http://localhost:8080/",
-		}
-
-		data, err := cl.Download(clipcode)
-		if err != nil {
-			log.Fatalln(err)
-		}
-
-		text, err := ioutil.ReadAll(data)
-		if err != nil {
-			log.Fatalln(err)
-		}
-
-		log.Println(string(text))
+		get(clipcode)
 
 	default:
 		invalid_usage()
+	}
+}
+
+func put(filename string) {
+	err := archiver.Archive([]string{filename}, ".clip.zip")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	file, err := os.Open(".clip.zip")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cl := client.Client{
+		BaseURL: "http://localhost:8080/",
+	}
+
+	clipcode, err := cl.Upload(file)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Printf("Recieved clipcode %s\n", clipcode)
+
+	err = os.Remove(".clip.zip")
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func get(clipcode string) {
+	cl := client.Client{
+		BaseURL: "http://localhost:8080/",
+	}
+
+	data, err := cl.Download(clipcode)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	file, err := os.Create(".clip.zip")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	_, err = io.Copy(file, data)
+	err = file.Close()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = archiver.Unarchive(".clip.zip", ".")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = os.Remove(".clip.zip")
+	if err != nil {
+		log.Fatalln(err)
 	}
 }
